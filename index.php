49215ee2c5b3f4cd8a4feb68ea9a00b7cb21cd97
Ararat-M <melkumyan2077@gmail.com>
2023-01-08 14:01:38 +0300

    update

--- index.php
+++ index.php
@@ -45,3 +45,78 @@ $example_persons_array = [
         'job' => 'babysitter',
     ],
 ];
+
+function getFullnameFromParts ($surname, $name, $patronomyc) {
+    return $surname . ' ' . $name . ' ' . $patronomyc;
+}
+
+function getPartsFromFullname($fullName) {
+    $arr = explode(' ', $fullName);
+
+    return ['surname' => $arr[0], 'name' => $arr[1], 'patronomyc' => $arr[2]];
+}
+
+function getShortName($fullName) {
+    $fullName = getPartsFromFullname($fullName);
+
+    return $fullName['name'] . ' ' . mb_substr($fullName['surname'], 0, 1) . '.';
+}
+
+function getGenderFromName($fullName) {
+    $fullName = getPartsFromFullname($fullName);
+    $gender = 0;
+    // Отчество
+    if (mb_substr($fullName['patronomyc'], -3) == 'вна') {
+        $gender--;
+    } else if (mb_substr($fullName['patronomyc'], -2) == 'ич') {
+        $gender++;
+    }
+    // Имя
+    if (mb_substr($fullName['name'], -1) == 'а') {
+        $gender--;
+    } else if (mb_substr($fullName['name'], -1) == 'й' || mb_substr($fullName['name'], -1) == 'н') {
+        $gender++;
+    }
+    // Фамилия
+    if (mb_substr($fullName['surname'], -2) == 'ва') {
+        $gender--;
+    } else if (mb_substr($fullName['surname'], -1) == 'в') {
+        $gender++;
+    }
+
+    if ($gender < 0) {
+        return 1;
+    } else if ($gender > 0) {
+        return -1;
+    } else {
+        return 0;
+    }
+}
+
+function getGenderDescription($personArray) {
+    $genderMaleCounter = 0;
+    $genderFemaleCounter = 0;
+    $genderindefinedCounter = 0;
+    for ($i=0; $i < count($personArray); $i++) {
+        $fullName = $personArray[$i]['fullname'];
+        $gender = getGenderFromName($fullName);
+        
+        if ($gender == 1) {
+            $genderFemaleCounter++;
+        }
+
+        if ($gender == -1) {
+            $genderMaleCounter++;
+        }
+        
+        if ($gender == 0) {
+            $genderindefinedCounter++;
+        }
+    }
+
+    echo 'Гендерный состав аудитории:' . '<br>';
+    echo '------------------------------------' . '<br>';
+    echo 'Мужчины - ' . round($genderMaleCounter * 100 / count($personArray), 1 ) . '%' . '<br>';
+    echo 'Женщины - ' . round($genderFemaleCounter * 100 / count($personArray), 1) . '%' .  '<br>';
+    echo 'Неопределенно - ' . round($genderindefinedCounter * 100 / count($personArray), 1) . '%' .  '<br>';
+}
